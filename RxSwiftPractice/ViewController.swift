981e04ec368ae2100cbf2c4dc79eb529a46b3d92
hikaru <hika49050@gmail.com>
2021-02-02 13:42:25 +0900

    Add feature(Increase count when button tapped)

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    let count = BehaviorRelay(value: 0)
+    let disposebag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        bindButtonToValue()
+        bindCountToText()
     }
 
+    func bindButtonToValue() {
+        button.rx.tap
+            .subscribe(onNext: { [weak self] _ in
+                self?.count.accept((self?.count.value)! + 1)
+            })
+            .disposed(by: disposebag)
+    }
+    
+    func bindCountToText() {
+        count.asObservable()
+            .subscribe(onNext: { [weak self] count in
+                self?.label.text = String(count)
+            })
+            .disposed(by: disposebag)
+    }
 
 }
 
