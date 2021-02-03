7a82b034987b0bc68837f7781d3a8da4915026f3
hikaru <hika49050@gmail.com>
2021-02-03 09:13:52 +0900

    Add feature(Reflect the value of textField on the label)

--- RxSwiftPractice/ViewController.swift
+++ RxSwiftPractice/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var label: UILabel!
+    
+    var viewModel: ViewModel?
+    let disposebag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        viewModel = ViewModel(inputText: textField.rx.text.orEmpty.asDriver())
+        
+        viewModel?.outputText
+            .drive(label.rx.text)
+            .disposed(by: disposebag)
     }
+}
 
-
+class ViewModel {
+    
+    let outputText: Driver<String>
+    
+    init(inputText: Driver<String>) {
+        self.outputText = inputText
+    }
 }
 
